fc065a4081acb49b674b71a664f7960a0009c468
Oskar Jorgensen <oskarjorgensen@gmail.com>
2022-11-23 22:05:45 +0100

    create the player class which stores player id, deck of player, cards on hand, and available energy

---
+++ Player.py
@@ -0,0 +1,45 @@
+import random
+
+from CONSTANTS import CARD_DICT, DECKSIZE
+
+from Card import Card
+from Location import Location
+from Deck import Deck
+
+class Player(object):
+    
+    def __init__(self, cardNames: list[str], playerIdx: int, availableEnergy: int = 0) -> None:
+        self.cardNames = cardNames
+        self.deck = Deck(cardNames)
+        self.hand = None
+        self.isStarting = None
+        self.availableEnergy = availableEnergy
+        self.playerIdx = playerIdx
+
+    def __str__(self) -> str:
+        return str(self.playerIdx)
+
+    def playIsLegal(self, card: Card, location: Location) -> bool:
+        if(not location.isPlayable(playerIdx=self.playerIdx)):
+            #print("The location is full or obstructed")
+            return False
+        if(card.cost > self.availableEnergy):
+            #print(f"You don't have enough energy to play this card ({card})")
+            return False
+        return True
+    
+    def selectMove(self, legalMoves: list[list[Card, Location]]) -> list[Card, Location]:
+        move = random.choice(legalMoves)
+        return move
+    
+    def playMove(self, legalMoves: list[list[Card, Location]]) -> bool:
+        move = self.selectMove(legalMoves)
+        if(move == None):
+            return None
+        card, location = move
+        if(not self.playIsLegal(card, location)):
+            print("This play is illegal")
+        location.addCard(card, self.playerIdx)
+        self.availableEnergy -= card.cost
+        return move
+        
