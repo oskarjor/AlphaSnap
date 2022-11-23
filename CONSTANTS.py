8155f7e8be030e325d877bbbd1315b6826c0b94b
Oskar Jorgensen <oskarjorgensen@gmail.com>
2022-11-23 22:02:06 +0100

    add constants for mapping card ids to classes and location ids to classes

---
+++ CONSTANTS.py
@@ -0,0 +1,16 @@
+from Location import RuinsLocation
+from Card import MistyKnight, Shocker, Cyclops, Abomination
+
+DECKSIZE = 12
+NUM_LOCATIONS = 3
+
+LOCATION_DICT = {
+    "ruins": RuinsLocation
+}
+
+CARD_DICT = {
+    "mistyKnight": MistyKnight, 
+    "shocker": Shocker, 
+    "cyclops": Cyclops, 
+    "abomination": Abomination, 
+}
