21aa54a55c0aedef78db58a2fa68eabbeeede7e5
Oskar Jorgensen <oskarjorgensen@gmail.com>
2022-11-24 17:58:25 +0100

    refactor and add function to location

--- Location.py
+++ Location.py
@@ -1,31 +1,55 @@
-from CardClass.Card import Card
+from Card import Card
 
 class Location(object):
 
-    def __init__(self, name: str, idx: int, freeSpaces: list[int, int], desc: str) -> None:
+    def __init__(self, name: str, idx: int, cardSpaces: list[int, int], desc: str) -> None:
         self.name = name
         self.idx = idx
-        self.freeSpaces = freeSpaces
+        self.cardSpaces = cardSpaces
         self.cards = [[], []]
-        self.totalPower = [0, 0]
         self.desc = desc
+        self.ongoinEnabled = True
+        self.onRevealEnabled = True
+        self.cardPlayedThisTurn = [False, False]
 
     def __str__(self) -> str:
         return f"{self.name} ({self.idx})"
-
-    def isPlayable(self, playerIdx) -> bool:
-        if(self.freeSpaces[playerIdx] <= 0):
-            print("This location is full")
-            return False
-        return True
     
     def addCard(self, card: Card, playerIdx: int):
+        if(not self.getPlayable(playerIdx=playerIdx)):
+            raise ValueError("Can't add more cards here")
         self.cards[playerIdx].append(card)
-        self.freeSpaces[playerIdx] -= 1
         card.atLocation = self
         card.playerIdx = playerIdx
+        self.cardPlayedThisTurn[playerIdx] = True
+
+    def triggerAllOngoing(self, playerIdx: int):
+        if(self.ongoinEnabled):
+            for card in self.cards[playerIdx]:
+                card.ongoing()
+
+
+    def triggerOnReveal(self, card: Card):
+        if(self.onRevealEnabled):
+            card.onReveal()
+
+
+    def removeCard(self, card: Card, playerIdx: int):
+        self.cards[playerIdx].remove(card)
+        self.triggerAllOngoing(playerIdx)
+    
+    def getTotalPower(self, playerIdx: int):
+        return sum([card.power for card in self.cards[playerIdx]])
+
+    def getAmountOfCards(self, playerIdx: int):
+        return len(self.cards[playerIdx])
+
+    def getPlayable(self, playerIdx: int):
+        if self.getAmountOfCards(playerIdx) < self.cardSpaces[playerIdx]:
+            return True
+        return False
 
 class RuinsLocation(Location):
 
     def __init__(self, idx: int):
-        super(RuinsLocation, self).__init__(name="Ruins", idx=idx, freeSpaces=[4, 4], desc="A ruined land")
+        super(RuinsLocation, self).__init__(name="Ruins", idx=idx, cardSpaces=[4, 4], desc="A ruined land")
