aae1f0bff5812de4bf7e8d15de0c07713eaae48a
Oskar Jorgensen <oskarjorgensen@gmail.com>
2022-11-27 01:35:14 +0100

    add basic location tests

--- test/LocationTest.py
+++ test/LocationTest.py
@@ -11,8 +11,11 @@ class TestLocations(unittest.TestCase):
         self.player0 = Player.Player([], playerIdx=0, availableEnergy=0)
         self.player1 = Player.Player([], playerIdx=1, availableEnergy=0)
 
-    def test_Location(self):
-        pass
-
     def test_RuinsLocation(self):
-        pass
+        # the location starts with 0 cards
+        self.assertEqual(self.location.getAmountOfCards(0), 0)
+        self.assertEqual(self.location.getAmountOfCards(1), 0)
+
+        # the location can store up to four cards
+        self.assertEqual(self.location.cardSpaces[0], 4)
+        self.assertEqual(self.location.cardSpaces[1], 4)
