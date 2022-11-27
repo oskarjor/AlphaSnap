6e3fa7a5d9e20a8975bd9033285c8be7f56a7edc
Oskar Jorgensen <oskarjorgensen@gmail.com>
2022-11-27 15:30:19 +0100

    add boilerplate test code for all major classes

---
+++ test/BoardTest.py
@@ -0,0 +1,24 @@
+import unittest
+
+import Player
+import Board
+import Location
+
+class TestBoard(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.board = Board.Board()
+        self.player0 = Player.Player([], playerIdx=0, availableEnergy=0)
+        self.player1 = Player.Player([], playerIdx=1, availableEnergy=0)
+
+    def test_Board(self) -> None:
+        for loc in self.board.locations:
+            self.assertEqual(loc, None)
+        self.assertEqual(len(self.board.locations), 3)
+
+        self.board.setupLocations()
+
+        for loc in self.board.locations:
+            self.assertNotEqual(loc, None)
+            self.assertIn(Location.Location, type(loc).__bases__)
+        self.assertEqual(len(self.board.locations), 3)
