fb97645f4faea388038fcdcff0096f234da4babf
Oskar Jorgensen <oskarjorgensen@gmail.com>
2022-11-27 18:18:35 +0100

    refactor CARD_DICT to use cost of cards

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,5 +1,5 @@
 from Location import RuinsLocation
-from Card import MistyKnight, Shocker, Cyclops, Abomination
+import Card
 
 DECKSIZE = 12
 NUM_LOCATIONS = 3
@@ -9,8 +9,48 @@ LOCATION_DICT = {
 }
 
 CARD_DICT = {
-    "mistyKnight": MistyKnight, 
-    "shocker": Shocker, 
-    "cyclops": Cyclops, 
-    "abomination": Abomination, 
+
+    "0-cost": 
+    {
+        "wasp": Card.Wasp, 
+        "yellowjacket": Card.Yellowjacket,
+    }, 
+
+    "1-cost": 
+    {
+        "mistyKnight": Card.MistyKnight, 
+        "antMan": Card.AntMan, 
+        "elektra": Card.Elektra, 
+        "mantis": Card.Mantis, 
+    }, 
+
+    "2-cost": 
+    {
+        "shocker": Card.Shocker, 
+        "starLord": Card.StarLord,
+    }, 
+
+    "3-cost": 
+    {
+        "cyclops": Card.Cyclops, 
+        "groot": Card.Groot, 
+    }, 
+
+    "4-cost": 
+    {
+        "abomination": Card.Abomination, 
+    }, 
+
 }
+
+def getFlatCardDict(CARD_DICT):
+    FLAT_CARD_DICT = {}
+    for _, COST_CARD_DICT in CARD_DICT.items():
+        for key, val in COST_CARD_DICT.items():
+            print(key, val)
+            FLAT_CARD_DICT[key] = val
+    return FLAT_CARD_DICT
+
+
+
+FLAT_CARD_DICT = getFlatCardDict(CARD_DICT)
--- Deck.py
+++ Deck.py
@@ -1,13 +1,13 @@
-from CONSTANTS import CARD_DICT
+from CONSTANTS import FLAT_CARD_DICT
 
 class Deck(object):
     def __init__(self, cardNames: list[str]) -> None:
         if(len(cardNames) != len(set(cardNames))):
             raise ValueError("Duplicate cards not allowed")
         for cardName in cardNames:
-            if(cardName not in CARD_DICT.keys()):
+            if(cardName not in FLAT_CARD_DICT.keys()):
                 raise ValueError("Invalid card name")
-        self.cards = [CARD_DICT[cardName]() for cardName in cardNames]
+        self.cards = [FLAT_CARD_DICT[cardName]() for cardName in cardNames]
     
     def __str__(self) -> str:
         return [str(card) for card in self.cards]
@@ -16,11 +16,3 @@ class Deck(object):
         if(len(self.cards) == 0):
             return
         return self.cards.pop(index)
-
-if __name__ == "__main__":
-    cardNamesLegal = ["mistyKnight", "abomination"]
-    cardNamesDuplicate = ["mistyKnight", "mistyKnight"]
-    cardNamesNonExisting = ["xxxxxxx"]
-    deckLegal = Deck(cardNamesLegal)
-    deckDuplicate = Deck(cardNamesDuplicate)
-    deckNonExisting = Deck(cardNamesNonExisting)
