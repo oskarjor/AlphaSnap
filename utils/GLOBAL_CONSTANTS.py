240ee7a88cba8b34086969d64f9309f0a5b03ea8
Oskar Jorgensen <oskarjorgensen@gmail.com>
2022-11-28 12:57:36 +0100

    change END_OF_TURN to AFTER_TURN

--- utils/GLOBAL_CONSTANTS.py
+++ utils/GLOBAL_CONSTANTS.py
@@ -7,7 +7,7 @@ NUM_LOCATIONS = 3
 TURN_STAGES = {
     "BEFORE_TURN": 0, 
     "DURING_TURN": 1, 
-    "END_OF_TURN": 2, 
+    "AFTER_TURN": 2, 
 }
 
 SAMPLE_DECK = ["wasp", "mistyKnight", "shocker", "starLord", "cyclops", "groot", "abomination"]
