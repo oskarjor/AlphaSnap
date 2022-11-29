e7720a47045a5434edd44a957302529cf11217f8
Oskar Jorgensen <oskarjorgensen@gmail.com>
2022-11-29 23:44:20 +0100

    add function for getting random card, removing card and discarding card from hand

--- Hand.py
+++ Hand.py
@@ -2,9 +2,9 @@ import random
 import Card
 
 class Hand(object):
-    def __init__(self) -> None:
+    def __init__(self, cards: list[Card.Card] = []) -> None:
         self.maxHandSize = 7
-        self.cards = []
+        self.cards = cards
     
     def __str__(self) -> str:
         return str([str(card) for card in self.cards])
@@ -25,7 +25,21 @@ class Hand(object):
         self.cards.append(card)
         return True
 
+    def getCards(self):
+        return self.cards.copy()
+
+    def getRandomCard(self):
+        return random.choice(self.cards.copy())
+
+    def discardCard(self, card: Card.Card):
+        if(self.removeCard(card=card)):
+            card.onDiscard()
+            return True
+        return False
+        
+
     def removeCard(self, card: Card.Card):
-        if(len(self.cards) == 0):
-            return
-        return self.cards.remove(card)
+        if(len(self.cards) == 0 or card not in self.cards):
+            return False
+        self.cards.remove(card)
+        return True
