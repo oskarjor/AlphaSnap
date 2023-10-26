48bf8b54a961686e6a423d2dfe27501f09d8d301
Oskar Jorgensen <oskarjorgensen@gmail.com>
2023-10-26 11:11:19 +0900

    add test to see if all cards are implemented

---
+++ test/ScraperTest.py
@@ -0,0 +1,33 @@
+import unittest
+import json
+import os
+
+import Card
+import Location
+import Player
+import Game
+import Board
+
+import utils.GLOBAL_CONSTANTS as GLOBAL_CONSTANTS
+from utils.utils import all_subclasses
+
+class TestScraper(unittest.TestCase):
+
+    def setUp(self) -> None:
+        path = GLOBAL_CONSTANTS.PATH_TO_CHARACHTER_STATS + GLOBAL_CONSTANTS.CURRENT_CHARACTER_SNAPSHOT
+        path = os.path.abspath(path)
+        with open(path) as json_file:
+            self.data = json.load(json_file)
+        sub = all_subclasses(Card.Card)
+        self.implemented_card_names = []
+        for s in sub:
+            try:
+                self.implemented_card_names.append(s().name.lower())
+            except:
+                pass
+
+    def test_all_cards_implemented(self):
+        print(self.implemented_card_names)
+        for key in self.data.keys():
+            if key.lower() not in self.implemented_card_names:
+                raise NotImplementedError(f"{key} not implemented")
