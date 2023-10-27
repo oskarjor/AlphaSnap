bb0a41da4098edf174318171095a750ed75713d4
Oskar Jorgensen <oskarjorgensen@gmail.com>
2023-10-27 11:55:35 +0900

    add gamehistory class

--- Game.py
+++ Game.py
@@ -5,13 +5,17 @@ from Board import Board
 import utils.GLOBAL_CONSTANTS
 from Card import Card
 from Location import Location
+import pprint
+from GameHistory import gameHistory
+from utils import GLOBAL_CONSTANTS
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     pass
 
+
 class Game(object):
-    
+
     def __init__(self, board: Board, player0: Player.Player, player1: Player.Player) -> None:
         self.board = board
         self.player0 = player0
@@ -20,7 +24,7 @@ class Game(object):
         self.stage = -1
 
         # a dict of everything that has happened during the game
-        # Format 
+        # Format
         # {
         #   turn : [event_1, event_2, event_3, event_4, ...]
         #   turn : [event_1, event_2, event_3, event_4, ...]
@@ -28,36 +32,30 @@ class Game(object):
         # }
         # event_i = [eventType, ...]
         #
-        # CARD ACTIONS: 
+        # CARD ACTIONS:
         # eventType = cardPlayed                ->      turn : ["cardPlayed", player, card, location]       (check)
         # eventType = cardRevealed              ->      turn : ["cardRevealed", player, card, location]     (check)
         # eventType = cardMoved                 ->      turn : ["cardMoved", player, card, fromLocation, toLocation]
         # eventType = cardDestroyed             ->      turn : ["cardDestroyed", player, card, location]
         # eventType = cardDiscarded             ->      turn : ["cardDiscarded", player, card, location]
-        # 
-        # LOCATION ACTIONS: 
+        #
+        # LOCATION ACTIONS:
         # eventType = locationRevealed          ->      turn : ["locationRevealed", location]
         # eventType = locationAbilityTriggered  ->      turn : ["locationAbilityTriggered", location]
-        # 
-        # GAME ACTIONS: 
+        #
+        # GAME ACTIONS:
         # eventType = turnStart                 ->      turn : ["turnStarted"]       (check)
         # eventType = turnEnd                   ->      turn : ["turnEnded"]         (check)
         # eventType = gameStarted               ->      turn : ["gameStarted"]       (check)
         # eventType = gameEnded                 ->      turn : ["gameEnded", result] (check)
 
-        self.gameHistory = {}
-
-    def addToPlayHistory(self, event: list):
-        if self.turn not in self.gameHistory.keys():
-            self.gameHistory[self.turn] = [event]
-        else:
-            self.gameHistory[self.turn].append(event)
+        self.gameHistory = gameHistory
 
     def getLegalMoves(self, player: Player.Player):
         legalMoves = []
         for card in player.hand.cards:
             for location in self.board.locations:
-                if(player.playIsLegal(card, location)):
+                if (player.playIsLegal(card, location)):
                     legalMoves.append([card, location])
         legalMoves.append(None)
         return legalMoves
@@ -66,14 +64,16 @@ class Game(object):
         self.turn = turn
         self.player0.availableEnergy = self.turn
         self.player1.availableEnergy = self.turn
-        self.addToPlayHistory(["turnStarted"])
+        event = GLOBAL_CONSTANTS.TURN_STARTED()
+        self.gameHistory.addEvent(turn=self.turn, event=event)
 
     def triggerAllLocationAbilities(self):
         for loc in self.board.locations:
             loc.locationAbility(self)
-    
+
     def startGame(self) -> None:
-        self.addToPlayHistory(["gameStarted"])
+        event = GLOBAL_CONSTANTS.GAME_STARTED()
+        self.gameHistory.addEvent(turn=self.turn, event=event)
         self.board.setupLocations()
 
     def beginTurn(self):
@@ -86,14 +86,14 @@ class Game(object):
             # TODO: (soon deprecated) cardPlayedThisTurn should use playHistory
             location.cardPlayedThisTurn = [False, False]
             location.turn = self.turn
-        
+
         self.player0.drawCard()
         self.player1.drawCard()
         player0Starts = self.board.playerIsStarting(player=self.player0)
         self.player0.isStarting = player0Starts == self.player0.playerIdx
         self.player1.isStarting = player0Starts == self.player1.playerIdx
+        # print("NUMCARDS", self.player0.hand.getNumCards())
 
-    
     def playTurn(self):
         self.stage = utils.GLOBAL_CONSTANTS.TURN_STAGES["DURING_TURN"]
         self.triggerAllLocationAbilities()
@@ -103,15 +103,15 @@ class Game(object):
             legalMoves = self.getLegalMoves(self.player0)
             moveToPlay = self.player0.selectMove(legalMoves)
             playedMove = self.player0.playMove(moveToPlay)
-            if(playedMove == None):
+            if (playedMove == None):
                 break
             player0PlayQueue.append(playedMove + [self.player0])
-        
+
         while True:
             legalMoves = self.getLegalMoves(self.player1)
             moveToPlay = self.player1.selectMove(legalMoves)
             playedMove = self.player1.playMove(moveToPlay)
-            if(playedMove == None):
+            if (playedMove == None):
                 break
             player1PlayQueue.append(playedMove + [self.player1])
 
@@ -123,7 +123,9 @@ class Game(object):
 
         for move in playQueue:
             card, location, player = move
-            self.addToPlayHistory(["cardPlayed", player, card, location])
+            event = GLOBAL_CONSTANTS.CARD_PLAYED(
+                player=player, card=card, location=location)
+            self.gameHistory.addEvent(turn=self.turn, event=event)
 
         return playQueue
 
@@ -133,10 +135,9 @@ class Game(object):
         for loc in self.board.locations:
             loc.triggerAllOngoing(self.player0.playerIdx, self)
             loc.triggerAllOngoing(self.player1.playerIdx, self)
-        self.addToPlayHistory(["turnEnded"])
+        event = GLOBAL_CONSTANTS.TURN_ENDED()
+        self.gameHistory.addEvent(turn=self.turn, event=event)
 
-
-    
     def revealCards(self, playQueue: list[Card, Location, Player.Player]):
         for card, location, player in playQueue:
             print(f"Player {player} played {card} at {location}!")
@@ -145,15 +146,16 @@ class Game(object):
 
     def endGame(self):
         gameWinner = self.board.playerIsWinning(player0)
-        if(gameWinner == 1):
-            self.addToPlayHistory(["gameEnded", player0])
+        if (gameWinner == 1):
+            event = GLOBAL_CONSTANTS.GAME_ENDED(result=player0)
             print("Player 0 wins!")
-        elif(gameWinner == -1):
-            self.addToPlayHistory(["gameEnded", player1])
+        elif (gameWinner == -1):
+            event = GLOBAL_CONSTANTS.GAME_ENDED(result=player1)
             print("Player 1 wins!")
-        elif(gameWinner == 0):
-            self.addToPlayHistory(["gameEnded", None])
+        elif (gameWinner == 0):
+            event = GLOBAL_CONSTANTS.GAME_ENDED(result=None)
             print("It's a tie!")
+        self.gameHistory.addEvent(turn=self.turn, event=event)
 
     def playGame(self):
         game.startGame()
@@ -170,7 +172,7 @@ class Game(object):
             print("-" * 124)
             print()
             self.visualizeBoard(self.board)
-            if(i == 6):
+            if (i == 6):
                 self.endGame()
                 continue
             play_next_turn = input("Do you wish to play next turn [y/n]: ")
@@ -180,6 +182,7 @@ class Game(object):
             elif play_next_turn.lower() == "n":
                 print("Game aborted")
                 break
+        # pprint.pprint(self.gameHistory)
 
     def visualizeBoard(self, board: Board):
         player0Cards = []
@@ -192,19 +195,23 @@ class Game(object):
             player1Cards[i] += [''] * (4 - len(player1Cards[i]))
             allLocations.append(location)
 
-
         print("Player 1 (opponent):")
         print("Hand:", player1.hand)
         print("-" * 124)
         for i in range(3, -1, -1):
-            print(f"| Spot {i+1} | {str(player1Cards[0][i]).center(35)} | {str(player1Cards[1][i]).center(35)} | {str(player1Cards[2][i]).center(35)} |")
+            print(f"| Spot {i+1} | {str(player1Cards[0][i]).center(35)} | {
+                  str(player1Cards[1][i]).center(35)} | {str(player1Cards[2][i]).center(35)} |")
         print("-" * 124)
-        print(f"|        | {str(allLocations[0].getTotalPower(1)).center(35)} | {str(allLocations[1].getTotalPower(1)).center(35)} | {str(allLocations[2].getTotalPower(1)).center(35)} |")
-        print(f"|        | {str(allLocations[0]).center(35)} | {str(allLocations[1]).center(35)} | {str(allLocations[2]).center(35)} |")
-        print(f"|        | {str(allLocations[0].getTotalPower(0)).center(35)} | {str(allLocations[1].getTotalPower(0)).center(35)} | {str(allLocations[2].getTotalPower(0)).center(35)} |")
+        print(f"| | {str(allLocations[0].getTotalPower(1)).center(35)} | {str(
+            allLocations[1].getTotalPower(1)).center(35)} | {str(allLocations[2].getTotalPower(1)).center(35)} |")
+        print(f"| | {str(allLocations[0]).center(35)} | {
+              str(allLocations[1]).center(35)} | {str(allLocations[2]).center(35)} |")
+        print(f"| | {str(allLocations[0].getTotalPower(0)).center(35)} | {str(
+            allLocations[1].getTotalPower(0)).center(35)} | {str(allLocations[2].getTotalPower(0)).center(35)} |")
         print("-" * 124)
         for i in range(0, 4, 1):
-            print(f"| Spot {i+1} | {str(player0Cards[0][i]).center(35)} | {str(player0Cards[1][i]).center(35)} | {str(player0Cards[2][i]).center(35)} |")
+            print(f"| Spot {i+1} | {str(player0Cards[0][i]).center(35)} | {
+                  str(player0Cards[1][i]).center(35)} | {str(player0Cards[2][i]).center(35)} |")
         print("-" * 124)
         print("Player 0 (you):")
         print("Hand:", player0.hand)
@@ -217,11 +224,7 @@ if __name__ == "__main__":
     board = Board()
     cardNames0 = random.sample(list(FLAT_CARD_DICT.keys()), 6)
     cardNames1 = random.sample(list(FLAT_CARD_DICT.keys()), 6)
-    print(cardNames0)
-    print(cardNames1)
     player0 = Player.Player(cardNames=cardNames0, playerIdx=0)
     player1 = Player.Player(cardNames=cardNames1, playerIdx=1)
     game = Game(board, player0, player1)
     game.playGame()
-    
-
---
+++ GameHistory.py
@@ -0,0 +1,78 @@
+class GameHistory(object):
+
+    def __init__(self) -> None:
+        # a dict of everything that has happened during the game
+        # Format
+        # {
+        #   turn : [event_1, event_2, event_3, event_4, ...]
+        #   turn : [event_1, event_2, event_3, event_4, ...]
+        #   turn : [event_1, event_2, event_3, event_4, ...]
+        # }
+        # event_i = [eventType, ...]
+        #
+        # CARD ACTIONS:
+        # eventType = cardPlayed                ->      turn : ["cardPlayed", player, card, location]       (check)
+        # eventType = cardRevealed              ->      turn : ["cardRevealed", player, card, location]     (check)
+        # eventType = cardMoved                 ->      turn : ["cardMoved", player, card, fromLocation, toLocation]
+        # eventType = cardDestroyed             ->      turn : ["cardDestroyed", player, card, location]
+        # eventType = cardDiscarded             ->      turn : ["cardDiscarded", player, card, location]
+        #
+        # LOCATION ACTIONS:
+        # eventType = locationRevealed          ->      turn : ["locationRevealed", location]
+        # eventType = locationAbilityTriggered  ->      turn : ["locationAbilityTriggered", location]
+        #
+        # GAME ACTIONS:
+        # eventType = turnStart                 ->      turn : ["turnStarted"]       (check)
+        # eventType = turnEnd                   ->      turn : ["turnEnded"]         (check)
+        # eventType = gameStarted               ->      turn : ["gameStarted"]       (check)
+        # eventType = gameEnded                 ->      turn : ["gameEnded", result] (check)
+        self.history = {}
+
+    def addEvent(self, turn: int, event: list) -> None:
+        if turn in self.history:
+            self.history[turn].append(event)
+        else:
+            self.history[turn] = [event]
+
+    def getLatestEventInGame(self, turn: int) -> list | None:
+        while turn > 0:
+            event = self.getLatestEventInTurn(turn=turn)
+            if event != None:
+                return event
+            else:
+                turn -= 1
+        return None
+
+    def getLatestEventInTurn(self, turn: int) -> list | None:
+        turnHistory = self.getTurnHistory(turn=turn)
+        if turnHistory:
+            return turnHistory[-1]
+        return None
+
+    def getTurnHistory(self, turn: int) -> list | None:
+        if turn in self.history:
+            return self.history[turn]
+        else:
+            return None
+
+    def getLatestEventOfTypeInGame(self, turn: int, eventType: str) -> list | None:
+        while turn > 0:
+            event = self.getLatestEventOfTypeInTurn(turn=turn)
+            if event != None:
+                return event
+            else:
+                turn -= 1
+        return None
+
+    def getLatestEventOfTypeInTurn(self, turn: int, eventType: str) -> list | None:
+        turnHistory = self.getTurnHistory(turn=turn)
+        if turnHistory == None:
+            return None
+        turnHistory.reverse()
+        for event in turnHistory:
+            if event[0] == eventType:
+                return event
+        return None
+
+
+gameHistory = GameHistory()
