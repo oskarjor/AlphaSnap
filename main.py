42aba4e3737deb7fd030d8ccb17b540f242a4f9f
Oskar Jorgensen <oskarjorgensen@gmail.com>
2023-10-28 18:51:13 +0900

    add main game loop to main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from utils.utils import all_subclasses
+from marvelsnap import Board, Player, Game
+from marvelsnap.GameHistory import gameHistory
+import random
 
 if __name__ == "__main__":
     import Card
-    sub = all_subclasses(Card.Card)
-    for s in sub:
-        try:
-            print(s().name)
-        except:
-            continue
+    FLAT_CARD_DICT = Card.getFlatCardDict()
+    board = Board()
+    cards0 = [card() for card in random.sample(
+        list(FLAT_CARD_DICT.values()), 6)]
+    cards1 = [card() for card in random.sample(
+        list(FLAT_CARD_DICT.values()), 6)]
+    player0 = Player.Player(cards=cards0, playerIdx=0)
+    player1 = Player.Player(cards=cards1, playerIdx=1)
+    game = Game(board, player0, player1)
+    game.playGame()
+    print(gameHistory)
