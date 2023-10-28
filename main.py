9f02ca8adf36945889f3e8797f8e895d255fc397
Oskar Jorgensen <oskarjorgensen@gmail.com>
2023-10-28 20:47:07 +0900

    fix imports in main

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from marvelsnap import Board, Player, Game
+from marvelsnap import Board, Player, Game, Card
 from marvelsnap.GameHistory import gameHistory
 import random
 
 if __name__ == "__main__":
-    import Card
     FLAT_CARD_DICT = Card.getFlatCardDict()
-    board = Board()
+    board = Board.Board()
     cards0 = [card() for card in random.sample(
         list(FLAT_CARD_DICT.values()), 6)]
     cards1 = [card() for card in random.sample(
         list(FLAT_CARD_DICT.values()), 6)]
     player0 = Player.Player(cards=cards0, playerIdx=0)
     player1 = Player.Player(cards=cards1, playerIdx=1)
-    game = Game(board, player0, player1)
+    game = Game.Game(board, player0, player1)
     game.playGame()
     print(gameHistory)
